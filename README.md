52fa69c0ef0fa1725bac527eae02e69c0fc2a003
Scott Woodend <woodendscott@gmail.com>
2017-12-10 17:55:16 -0500

    initial commit and webpack setup

---
+++ README.md
@@ -0,0 +1,5 @@
+Stock Tickr
+===============
+
+- Enter a stock symbol to view the monthly closing price for the past year
+- Uses websockets so this can be a collaborative process
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: 'bundle.js'
+    },
+    target: "node",
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+
+            },
+            {
+                test: /.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(gif|png|jpg|svg|jpeg)$/,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
+            }
+
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+}
