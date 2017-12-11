27ea4233d38e95b8799be9dad55e628438d60659
Scott Woodend <woodendscott@gmail.com>
2017-12-11 11:32:30 -0500

    create a component to eventually render a display of which stocks are currently being displayed. User will be able to cancel the display by clicking an x on the stock

---
+++ src/components/CurrentStocksPanel.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/currentstockspanel.css';
+
+export default class CurrentStocksPanel extends Component {
+    render(){
+        return (
+            <div className="current-stocks-panel">
+
+            </div>
+        )
+    }
+}
