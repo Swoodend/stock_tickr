3ce7a3d8ab38e998eeb459c27a9760593dc3bd39
Scott Woodend <woodendscott@gmail.com>
2017-12-11 17:04:19 -0500

    set some dummy data for initial reducer state to be drawn out on the chart for testing purposes. In the future this data will likely be fetched from an api and sent to the appropriate components

--- src/reducers/stockManipulator.js
+++ src/reducers/stockManipulator.js
@@ -1,4 +1,28 @@
-const initialState = {foo: "bar"};
+const initialState = {
+    chartData : {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Monthly Closing Price'
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar']
+        },
+        yAxis: {
+            title: {
+                text: 'Price Per Share (USD)'
+            }
+        },
+        series: [{
+            name: 'GOOG',
+            data: [1, 0, 4]
+        }, {
+            name: 'AAPL',
+            data: [5, 7, 3]
+        }]
+    }
+};
 
 export function stockManipulator(state = initialState, action){
     switch (action.type){
