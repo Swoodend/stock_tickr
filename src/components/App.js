c779ea2ca1172b812493ad0c8e981e84f01f17f8
Scott Woodend <woodendscott@gmail.com>
2017-12-11 17:01:35 -0500

    App component now pulls state from redux store. Chart data is passed to the MainChart component as props. This data is used to draw the chart. Current stock data is pulled from the chart data and passed to CurrentstocksPanel component. This component will eventually render out tags of each stock symbol that can be removed from the chart when clicked upon.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import MainTitle from './MainTitle';
 import MainChart from './MainChart';
 import CurrentStocksPanel from './CurrentStocksPanel';
 import StockInputField from './StockInputField';
 import '../styles/app.css';
 
+@connect((store) => {
+    return {
+        chartData: store.chartData
+    }
+})
 export default class App extends Component {
     render(){
+        let currentStocks = this.props.chartData.series.map(series => series.name);
         return (
             <div className="app">
                 <MainTitle/>
-                <MainChart/>
-                <CurrentStocksPanel/>
+                <MainChart chartData={this.props.chartData}/>
+                <CurrentStocksPanel currentStocks={currentStocks}/>
                 <StockInputField/>
             </div>
         );  
