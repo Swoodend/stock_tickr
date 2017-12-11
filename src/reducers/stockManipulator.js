fec12fcb6368becfe1a8d1db546128a280ce9fab
Scott Woodend <woodendscott@gmail.com>
2017-12-11 16:28:04 -0500

    creates reducer

---
+++ src/reducers/stockManipulator.js
@@ -0,0 +1,10 @@
+const initialState = {foo: "bar"};
+
+export function stockManipulator(state = initialState, action){
+    switch (action.type){
+
+        default: 
+            return state;
+
+    }
+}
