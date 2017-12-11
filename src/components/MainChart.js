a60ce704b46fbed2beacc7d84eee16ce83b18643
Scott Woodend <woodendscott@gmail.com>
2017-12-11 17:02:50 -0500

    chart config is now passed as props to the MainChart component from the redux store

--- src/components/MainChart.js
+++ src/components/MainChart.js
@@ -2,34 +2,11 @@ import React, { Component } from 'react';
 import ReactHighcharts from 'react-highcharts';
 import '../styles/mainchart.css';
 
-export default class MainChart extends Component {
+export default class MainChart extends Component { 
     render(){
-        let config = {
-            chart: {
-                type: 'line'
-            },
-            title: {
-                text: 'Monthly Closing Price'
-            },
-            xAxis: {
-                categories: ['Jan', 'Feb', 'Mar']
-            },
-            yAxis: {
-                title: {
-                    text: 'Price (millions)'
-                }
-            },
-            series: [{
-                name: 'GOOG',
-                data: [1, 0, 4]
-            }, {
-                name: 'AAPL',
-                data: [5, 7, 3]
-            }]
-        }
         return (
             <div className="main-chart-container">
-                <ReactHighcharts config={config} domProps={{style: {height: "100%", width:"100%"}}}/>
+                <ReactHighcharts config={this.props.chartData} domProps={{style: {height: "100%", width:"100%"}}}/>
             </div>
         )
     }
