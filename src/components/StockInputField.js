066c85697eb49d35042b10a16841e653e6791b92
Scott Woodend <woodendscott@gmail.com>
2017-12-11 11:37:49 -0500

    component renders out an input field for user to enter stock symbols into

---
+++ src/components/StockInputField.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import '../styles/stockinputfield.css';
+
+export default class StockInputField extends Component {
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        let stockSymbol = e.target.stockSymbol.value;
+        //do some redux/socketio stuff here
+    }
+
+    render(){
+        return (
+            <div className="stock-input-field-container">
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="stockSymbol" placeholder=">Enter a stock symbol"/>
+                    <button id="stock-submit" type="submit">Add Stock</button>
+                </form>
+            </div>
+        )
+    }
+}
