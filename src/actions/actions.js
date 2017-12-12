d1b65ad15569b08692d600d70c47bde77cc0ce19
Scott Woodend <woodendscott@gmail.com>
2017-12-12 17:49:24 -0500

    getInitialState() uses helper function formatDataForHighcharts to format the collected API data for the highcharts chart.

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -14,20 +14,58 @@ export function getData(symbol){
     )
 }
 
-export function getInitialState(){
-    //want to have some stock data displayed to the user on initial page load.
-    //this fn will get stock prices for GOOG and AAPL
+export function getInitialState(year){
+
     return dispatch => {
         Promise.all([getData('GOOG'), getData('AAPL')])
-        .then(([goog, aapl]) => {
-            console.log("promises have resolved with:");
-            console.log("GOOG", goog);
-            console.log("AAPL", aapl);
+        .then(([allGoogleData, allAppleData]) => {
+
+           let googSeriesObj = formatDataForHighcharts(allGoogleData, year);
+           let aaplSeriesObj = formatDataForHighcharts(allAppleData, year);
             //dispatch some action here
+
         }).catch((e) => {
             console.log(e);
             //dispatch error handling action here
         })
     }
+
+}
+
+export function formatDataForHighcharts(apiResponse, year="2017"){
+    //takes in apiResponse (obj literal) and formats for usage by highchartsApi    
+    let symbol = apiResponse['Meta Data']['2. Symbol'];
+
+    //get all the data keys for year 2017, you could allow the user to choose a year    
+    let allDataKeysForYear = Object.keys(apiResponse['Weekly Adjusted Time Series']).filter(date => {
+        return date.indexOf(year) === 0;
+    });
+
+    //get MOST recent keys for each month in 2017 (we want the latest data for each month)
+    let mostRecentKeys = allDataKeysForYear.reduce((hash, date) => {
+        let dateSubstr = date.slice(0, 7);
+        if(hash[dateSubstr]){
+            return hash;
+        } else {
+            hash[dateSubstr] = date;
+            return hash;
+        }
+    }, {});
+
+    //want the values from mostRecentKeysHash
+    let recentKeysArr = Object.keys(mostRecentKeys).map(date => {
+        return mostRecentKeys[date];
+    });
+
+    let monthlyValues = recentKeysArr.map( date => {
+        return [apiResponse['Weekly Adjusted Time Series']][0][date]['4. close'];
+    });
+
+    //these objects get put into the series array
+    return {
+        name: symbol,
+        data: monthlyValues
+    }
+
 
 }
