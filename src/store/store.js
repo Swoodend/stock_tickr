452c148ada0b5cb90659ab32ccfdb51dcf47cf2a
Scott Woodend <woodendscott@gmail.com>
2017-12-12 10:12:45 -0500

    require the thunk middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { stockManipulator as reducer } from '../reducers/stockManipulator';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
-let middlewares = applyMiddleware(logger);
+let middlewares = applyMiddleware(logger, thunk);
 export let store = createStore(reducer, middlewares);
