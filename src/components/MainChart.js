3b5ea5721c197e03d48eed75ff703724b86c666a
Scott Woodend <woodendscott@gmail.com>
2017-12-13 12:07:35 -0500

    formatting - add line breaks

--- src/components/MainChart.js
+++ src/components/MainChart.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import ReactHighcharts from 'react-highcharts';
 import '../styles/mainchart.css';
 
-export default class MainChart extends Component { 
+export default class MainChart extends Component {
+    
     render(){
         return (
             <div className="main-chart-container">
-                <ReactHighcharts config={this.props.chartData} domProps={{style: {height: "100%", width:"100%"}}}/>
+                <ReactHighcharts 
+                    config={this.props.chartData} 
+                    domProps={{style: {height: "100%", width:"100%"}}}
+                    ref="chart"
+                />
             </div>
         )
     }
