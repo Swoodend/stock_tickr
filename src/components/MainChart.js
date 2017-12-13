d453f2d4fc7c1a485bceee734bf08b60a6e306c3
Scott Woodend <woodendscott@gmail.com>
2017-12-13 12:45:28 -0500

    hacky way to change the title of the chart depending on if the chart is displaying data or not. If it is, the title is set. If it is not display data, the title defaults to 'Enter a stock below'. This can be handled by redux later

--- src/components/MainChart.js
+++ src/components/MainChart.js
@@ -3,14 +3,18 @@ import ReactHighcharts from 'react-highcharts';
 import '../styles/mainchart.css';
 
 export default class MainChart extends Component {
-    
+    componentWillReceiveProps(nextProps){
+        if (nextProps.chartData.series.length > 0){
+            nextProps.chartData.title.text = 'Monthly Closing Price Per Share'
+        }
+    }
+
     render(){
         return (
             <div className="main-chart-container">
                 <ReactHighcharts 
                     config={this.props.chartData} 
                     domProps={{style: {height: "100%", width:"100%"}}}
-                    ref="chart"
                 />
             </div>
         )
