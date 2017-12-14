88b7b25d71a10143ed44a5373123dee45765b2f7
Scott Woodend <woodendscott@gmail.com>
2017-12-14 09:42:31 -0500

    appState reducer now udpates state upon receiving UPDATE_INPUT_VALUE action or UPDATE_CHART_TITLE

--- src/reducers/appState.js
+++ src/reducers/appState.js
@@ -6,9 +6,14 @@ const initialState = {
 function appState(state = initialState, action){
     switch(action.type){
         case "UPDATE_INPUT_VALUE":
+            return Object.assign({}, state, {
+                stockInputValue: action.payload
+            })
 
         case "UPDATE_CHART_TITLE":
-
+            return Object.assign({}, state, {
+                chartTitle: action.payload
+            })
         default: 
             return state;
     }
