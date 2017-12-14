eee7546225358134511d8a8bce35c8aa39ccf5d8
Scott Woodend <woodendscott@gmail.com>
2017-12-14 10:32:32 -0500

    If the chart is not displaying any stocks, switch the title

--- src/components/MainChart.js
+++ src/components/MainChart.js
@@ -6,6 +6,8 @@ export default class MainChart extends Component {
     componentWillReceiveProps(nextProps){
         if (nextProps.chartData.series.length > 0){
             nextProps.chartData.title.text = 'Monthly Closing Price Per Share'
+        } else if (nextProps.chartData.series.length === 0) {
+            nextProps.chartData.title.text = 'Enter a stock below'
         }
     }
 
