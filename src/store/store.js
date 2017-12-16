1b07ce86c3ae8a4e4b700a6d5b6ad094b98c6cb2
Scott Woodend <woodendscott@gmail.com>
2017-12-15 21:25:43 -0500

    redux store must user socket middleware and setup initial socket logic/event handlers

--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,20 @@ import { stockManipulator } from '../reducers/stockManipulator';
 import { appState } from '../reducers/appState';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { socketMiddleware } from '../middlewares/socketMiddleware';
+import io from 'socket.io-client';
+import { wsAddStock, wsRemoveStock } from '../actions/actions';
 
-let middlewares = applyMiddleware(logger, thunk);
+const socket = io();
+socket.on('new stock', stockObj => {
+    store.dispatch(wsAddStock(stockObj))
+});
+
+socket.on('removed stock', symbol => {
+    store.dispatch(wsRemoveStock(symbol));
+})
+
+let middlewares = applyMiddleware(logger, thunk, socketMiddleware(socket));
 let reducers = combineReducers({
     'appState': appState,
     'chartData': stockManipulator
