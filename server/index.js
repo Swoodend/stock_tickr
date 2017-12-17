09958a6bd6963928c3211a690903b3286cd53f64
Scott Woodend <woodendscott@gmail.com>
2017-12-17 15:55:21 -0500

    connect to db, socket handlers now update the DB accordingly. This was done in order to allow new visitors to the site to be able to see the current state of the chart as built by everyone else prior to them joining

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const port = 3000;
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+
 app.use(express.static(path.join(__dirname, '/../dist')));
 
 
@@ -14,21 +16,54 @@ const server = app.listen(port, () => {
 
 const io = require('socket.io')(server);
 
-io.on('connection', (socket) => {
-    console.log(socket.id, 'connected');
 
-    socket.on('disconnect', () => {
-        console.log(socket.id, 'disconnected');
-    })
+    
+MongoClient.connect('mongodb://localhost:27017/', (err, db) => {
+    if (err){ return console.dir(err); }
+    let dbase = db.db('stock_tickr');        
+    dbase.createCollection('current_series_state', (err, collection) => {
+        if (err){ return console.dir(err); }
+        let clients = [];     
+        io.on('connection', (socket) => {
+            clients.push(socket.id);
+            socket.on('update chart state', () => {
+                collection.find({}, {name: 1, data: 1,   _id: 0}).toArray((err, result) => {
+                    io.sockets.connected[clients[clients.length - 1]].emit('update chart state', result)                
+                });
+            })
 
-    socket.on('add stock', stockObj => {
-        io.emit('new stock', stockObj);
-    });
+            socket.on('disconnect', () => {
+                let clientIndex = clients.findIndex((id) => {
+                    return id === socket.id
+                });
+                clients.splice(clientIndex, 1);
 
-    socket.on('remove stock', symbol => {
-        io.emit('removed stock', symbol);
-    })
+                if (!clients.length){
+                    collection.remove({});
+                }
+            })
+
+            socket.on('add stock', stockObj => {
+                collection.find({name: stockObj.name}).toArray((err, result) => {
+                    if (err) { return console.dir(err)};
+                    if (result.length === 0){
+                        collection.insert(stockObj);
+                        io.emit('new stock', stockObj);
+                    }
+                })
+
+            });
+
+            socket.on('remove stock', symbol => {
+                collection.remove({name: symbol});
+                console.log('removied');
+                io.emit('removed stock', symbol);
+            });
+        
+        });
+    }); 
 });
+
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, '/../dist/index.html'));
