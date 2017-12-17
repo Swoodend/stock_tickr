c545fddc312aaf53c6cb3b2f7150243b7bc952ee
Scott Woodend <woodendscott@gmail.com>
2017-12-17 15:55:44 -0500

    fire a new connection action when user visits the site

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { fireNewConnection } from './actions/appState';
 
-
+store.dispatch(fireNewConnection());
 ReactDOM.render(
     <Provider store={store}>
         <App/>
